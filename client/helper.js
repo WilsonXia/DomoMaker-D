bf4703236c70e760e21dcf536231c267056935d0
WilsonXia <wx4477@rit.edu>
2024-11-12 17:09:39 -0500

    finished domomaker d

---
+++ client/helper.js
@@ -0,0 +1,42 @@
+const handleError = (message) => {
+    document.getElementById('errorMessage').textContent = message;
+    document.getElementById('domoMessage').classList.remove('hidden');
+};
+
+const hideError = () => {
+    document.getElementById('domoMessage').classList.add('hidden');
+}
+
+/* Sends post requests to the server using fetch. Will look for various
+   entries in the response JSON object, and will handle them appropriately.
+*/
+const sendPost = async (url, data, handler) => {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    const result = await response.json();
+    hideError();
+
+    if (result.redirect) {
+        window.location = result.redirect;
+    }
+
+    if (result.error) {
+        handleError(result.error);
+    }
+
+    if (handler) {
+        handler(result);
+    }
+};
+
+module.exports = {
+    hideError,
+    handleError,
+    sendPost
+}
--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -2,17 +2,20 @@ const models = require('../models');
 
 const { Domo } = models;
 
-const makerPage = async (req, res) => {
-  try {
-    const query = { owner: req.session.account._id };
+const makerPage = (req, res) => {
+  return res.render('app');
+};
+
+const getDomos = async (req, res) => {
+  try{
+    const query = {owner: req.session.account._id};
     const docs = await Domo.find(query).select('name age').lean().exec();
 
-    return res.render('app', { domos: docs });
+    return res.json({domos: docs});
   } catch (err) {
     console.log(err);
-    return res.status(500).json({ error: 'Error retrieving domos!' });
+    return res.status(500).json({error: 'Error retrieving documents!'});
   }
-  // res.render('app');
 };
 
 const makeDomo = async (req, res) => {
@@ -29,7 +32,7 @@ const makeDomo = async (req, res) => {
   try {
     const newDomo = new Domo(domoData);
     await newDomo.save();
-    return res.json({ redirect: '/maker' });
+    return res.status(201).json({ name: newDomo.name, age: newDomo.age });
   } catch (err) {
     console.log(err);
     if (err.code === 11000) {
@@ -42,4 +45,5 @@ const makeDomo = async (req, res) => {
 module.exports = {
   makerPage,
   makeDomo,
+  getDomos,
 };
